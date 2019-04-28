3fa349c78a9860eed233de01217f984eced32e43
DeividasK12 <Deividas.Kvedaras@stud.vgtu.lt>
2019-04-28 22:10:33 +0300

    Made main functionality

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,97 @@
 const path = require('path');
 const express = require('express');
+const fetch = require('node-fetch');
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
-app.get('*', (_req, res) => {
+
+app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
+
+app.get('/getTeam/:teamName', (req, res) => {
+  fetch('https://www.balldontlie.io/api/v1/teams/')
+    .then(response => response.json())
+    .then((response) => {
+      res.send(response.data[1]);
+    });
+});
+
+
+// //////////////////////////////////////////////////////////////////////
+
+
+async function getPlayers(teamID) {
+
+  try {
+    return new Promise((resolve) => {
+      let players = {
+        data: [],
+      };
+      for (let i = 1; i < 3; i++) {
+        fetch(`https://www.balldontlie.io/api/v1/players?per_page=100&page=${i}`)
+          .then(response => response.json())
+          .then((response) => {
+            for (let key in response.data) {
+              if (response.data[key].team.id === parseInt(teamID, 10)) {
+                let playerObj = {
+                  id: response.data[key].id,
+                  firstName: response.data[key].first_name,
+                  lastName: response.data[key].last_name,
+                  team: response.data[key].team.id,
+                };
+                players.data.push(playerObj);
+              }
+            }
+          });
+      }
+      return setTimeout(() => {
+        resolve(players);
+      }, 3000);
+    });
+  } catch (er) {
+    console.log(er);
+  }
+}
+
+app.get('/players/:teamID', async (req, res) => {
+  let players = await getPlayers(req.params.teamID);
+  res.send(players);
+});
+
+
+// //////////////////////////////////////////////////////////////////////
+
+app.get('/teams', (req, res) => {
+  fetch('https://www.balldontlie.io/api/v1/teams/')
+    .then(response => response.json())
+    .then((response) => {
+      let teams = {
+        data: [],
+      };
+      for (let key in response.data) {
+        let teamObj = {
+          id: response.data[key].id,
+          full_name: response.data[key].full_name,
+        };
+        teams.data.push(teamObj);
+      }
+      res.send(teams);
+    });
+});
+
+/* app.get('/getTeam/:teamID', (req, res) => {
+  fetch(`https://www.balldontlie.io/api/v1/teams/${req.params.teamID}`)
+    .then(response => response.json())
+    .then((data) => {
+      res.send(data);
+      console.log(data);
+    });
+}); */
+
 
 app.listen(port, (error) => {
   if (error) {
--- frontend/src/Page.jsx
+++ frontend/src/Page.jsx
@@ -1,11 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class Page extends React.Component {
+class Page extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      teamsInfo: '',
+      playersInfo: '',
+      choosedTeam: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getTeams();
+    if (this.state.choosedTeam != null) {
+      this.getPlayers(this.state.choosedTeam);
+    }
+  }
+
+  getTeams() {
+    setTimeout(() => {
+      const teamList = [];
+      fetch('/teams')
+        .then(response => response.json())
+        .then((response) => {
+          this.state.isLoaded = true;
+          this.state.teamsInfo = response;
+          for (let i = 0; i < response.data.length; i++) {
+            teamList[i] = response.data[i].full_name;
+          }
+          this.setState({
+            teamsInfo: teamList,
+          });
+        });
+    }, 20);
+  }
+
+  getPlayers() {
+    setTimeout(() => {
+      const playerList = [];
+      fetch(`/players/${this.state.choosedTeam}`)
+        .then(response => response.json())
+        .then((response) => {
+          this.state.isLoaded = true;
+          this.state.playersInfo = response;
+          for (let i = 0; i < response.data.length; i++) {
+            playerList[i] = response.data[i].firstName.concat(' ', response.data[i].lastName);
+          }
+          this.setState({
+            playersInfo: playerList,
+          });
+        });
+    }, 20);
+  }
+
+  showChoosedTeam(e, id, teamID) {
+    this.state.choosedTeam = parseInt(id.key, 10) + 1;
+    this.componentDidMount();
   }
 
   render() {
-    return <div>Template</div>;
+    const { teamsInfo, playersInfo } = this.state;
+    return (
+      // eslint-disable-next-line max-len,react/no-array-index-key,react/button-has-type
+      <div>
+        <div id="teams">{Array.from(teamsInfo)
+          .map((team, key) => <button key={key} value={key} onClick={(e) => {
+            this.showChoosedTeam(e, { key }, { team });
+          }}>{team}</button>)}
+        </div>
+        <div id="players">
+          {Array.from(playersInfo)
+            .map((player, key) => <li key={key}>{player}</li>)}
+        </div>
+      </div>
+    );
   }
 }
+
+
+export default Page;
